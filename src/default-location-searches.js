b78264e37495026f36ecd402408371402163a4a8
ouisharelux <ouisharelux@gmail.com>
2022-02-28 21:11:50 -0500

    Change default location to New York

--- src/default-location-searches.js
+++ src/default-location-searches.js
@@ -9,38 +9,17 @@ const { LatLng, LatLngBounds } = sdkTypes;
 // `predictionPlace` (util.types.place) properties.
 const defaultLocations = [
   {
-    id: 'default-helsinki',
+    id: 'default-newyork',
     predictionPlace: {
-      address: 'Helsinki, Finland',
-      bounds: new LatLngBounds(new LatLng(60.29783, 25.25448), new LatLng(59.92248, 24.78287)),
+      address: 'New York, New York',
+      bounds: new LatLngBounds(new LatLng(40.830104, -73.939626), new LatLng(40.701595, -74.015538)),
     },
   },
   {
-    id: 'default-turku',
+    id: 'default-miami',
     predictionPlace: {
-      address: 'Turku, Finland',
-      bounds: new LatLngBounds(new LatLng(60.53045, 22.38197), new LatLng(60.33361, 22.06644)),
-    },
-  },
-  {
-    id: 'default-tampere',
-    predictionPlace: {
-      address: 'Tampere, Finland',
-      bounds: new LatLngBounds(new LatLng(61.83657, 24.11838), new LatLng(61.42728, 23.5422)),
-    },
-  },
-  {
-    id: 'default-oulu',
-    predictionPlace: {
-      address: 'Oulu, Finland',
-      bounds: new LatLngBounds(new LatLng(65.56434, 26.77069), new LatLng(64.8443, 24.11494)),
-    },
-  },
-  {
-    id: 'default-ruka',
-    predictionPlace: {
-      address: 'Ruka, Finland',
-      bounds: new LatLngBounds(new LatLng(66.16997, 29.16773), new LatLng(66.16095, 29.13572)),
+      address: 'Miami, Florida',
+      bounds: new LatLngBounds( new LatLng(25.7616798, -80.1917902), new LatLng(25.7823907, -80.2994991)),
     },
   },
 ];
